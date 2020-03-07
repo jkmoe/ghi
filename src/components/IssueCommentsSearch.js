1590da541815d1d481111d3e30f35418ea6b867b
Jannik Molter <jkmo@tutanota.com>
2020-03-07 21:52:21 +0100

    Made the rendering of issue comments conditional;

--- src/components/IssueCommentsSearch.js
+++ src/components/IssueCommentsSearch.js
@@ -44,6 +44,7 @@ export default function IssueCommentsSearch(props) {
         setFilteredComments(comments);
     };
 
+    if (!comments.length) return <></>;
     return (
         <>
             <Paper className={classes.searchRoot}>
